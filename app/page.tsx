0def2b27bed48ce914ae36f8e04722728de8b4d5
SalahVincent <vincentsalah3@gmail.com>
2026-03-13 15:16:09 +0100

    - implement: firebase admin session verification and client-side auth provider - add: google login - fix: drizzle config to load environment variables and sync database tables

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,36 @@
+"use client";
+
+import LoginButton from "@/app/login/page";
+import { useAuth } from "@/context/auth-context";
+import { auth } from "@/lib/firebase"
+
 export default function Home() {
+  const { user, loading } = useAuth();
+
+  if (loading) return <p className="p-8">Loading session...</p>;
+
   return (
-    <main className="p-8">
-      <h1 className="text-2xl font-bold">Todo Application</h1>
-      <p>Organize your day</p>
-      <button className="color-red-500">LOGIN</button>
+    <main className="p-8 flex flex-col items-center gap-6">
+      <h1 className="text-3xl font-bold">Todo App Test</h1>
+      
+      {!user ? (
+        <div className="text-center">
+          <p className="mb-4 text-gray-600">You are not logged in.</p>
+          <LoginButton />
+        </div>
+      ) : (
+        <div className="bg-green-50 border border-green-200 p-6 rounded-lg text-center">
+          <p className="text-green-700 font-medium">Logged in as:</p>
+          <p className="text-sm text-green-600 mb-4">{user.email}</p>
+          <p className="text-xs text-gray-400">UID: {user.uid}</p>
+          <button 
+            onClick={() => auth.signOut()} 
+            className="mt-4 text-sm text-red-600 underline"
+          >
+            Sign Out
+          </button>
+        </div>
+      )}
     </main>
-  )
+  );
 }
